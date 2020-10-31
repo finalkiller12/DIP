1e4f931194791669d64dcd335cd9e63bea2344cf
gerald <geraldtan97@gmail.com>
2020-10-31 17:03:54 +0800

    created functions to dynamically generate probabilities

--- MainWindow.py
+++ MainWindow.py
@@ -3,10 +3,12 @@ from PyQt5.QtCore import *
 from pandastable import Table
 from tkinter import filedialog
 from PyQt5.QtWidgets import QMainWindow, QApplication
-from DIP.Ui_MainWindow import Ui_MainWindow
+from Ui_MainWindow import Ui_MainWindow
 import pandas as pd
 import matplotlib.pyplot as plt
 from tkinter import *
+import datetime
+import math
 
 
 # import sys
@@ -42,34 +44,73 @@ class MainWindow():
     def show(self):
         self.main_win.show()
 
+    def loadTemperatureData(self):
+        data = pd.read_csv('Overview200.csv')
+        df = data.drop_duplicates(subset=['DeviceTimeStamp'])
+        df2 = df.loc[df['OTI'] >= 200]
+        # removed all non failure data points
+        return df2
+
+    def calcInterval(self, df2):
+        # calculate interval between failure data points
+
+        found = []
+        i = 0
+        for index, row in df2.iterrows():
+            if i > 0:
+                diff = datetime.datetime.strptime(df2.iloc[i].DeviceTimeStamp,
+                                                  '%Y-%m-%dT%H:%M') - datetime.datetime.strptime(
+                    df2.iloc[i - 1].DeviceTimeStamp, '%Y-%m-%dT%H:%M')
+
+                # less than 1 sec apart = redundant rows
+                if diff > datetime.timedelta(seconds=1):
+                    found.append([i, diff, index, row.DeviceTimeStamp])
+            i += 1
+
+        # only count failures that happened > 1 day apart. multiple failures count as 1 incident
+        time_list = []
+        for a in found:
+            if (a[1] > datetime.timedelta(days=1)):
+                # print(a)
+                time_list.append(a[1])
+
+        interval = pd.Series(time_list).median().days
+        return interval
+
+    def poisson_probs(self, interval):
+
+        pred_yst = 0
+        prob_list = []
+        for i in range(30):
+            pred_today = 1 - pow(math.e, -1 / interval * i)
+            prob_list.append(pred_today - pred_yst)
+            pred_yst = pred_today
+
+        return prob_list
+
     def show_Probabilty(self, date):
-        if date == QDate(2020, 10, 15):
-            self.ui.label_3.setText(date.toString() + ' --- 10.5%')
-        elif date == QDate(2020, 10, 16):
-            self.ui.label_3.setText(date.toString() + ' --- 9.41%')
-        elif date == QDate(2020, 10, 17):
-            self.ui.label_3.setText(date.toString() + ' --- 8.42%')
-        elif date == QDate(2020, 10, 18):
-            self.ui.label_3.setText(date.toString() + ' --- 7.54%')
-        elif date == QDate(2020, 10, 19):
-            self.ui.label_3.setText(date.toString() + ' --- 6.74%')
-        elif date == QDate(2020, 10, 20):
-            self.ui.label_3.setText(date.toString() + ' --- 6.03%')
-        elif date == QDate(2020, 10, 21):
-            self.ui.label_3.setText(date.toString() + ' --- 5.40%')
-        elif date == QDate(2020, 10, 22):
-            self.ui.label_3.setText(date.toString() + ' --- 4.83%')
-        elif date == QDate(2020, 10, 23):
-            self.ui.label_3.setText(date.toString() + ' --- 4.32%')
-        elif date == QDate(2020, 10, 24):
-            self.ui.label_3.setText(date.toString() + ' --- 3.87%')
-        elif date == QDate(2020, 10, 25):
-            self.ui.label_3.setText(date.toString() + ' --- 3.46%')
-        elif date == QDate(2020, 10, 26):
-            self.ui.label_3.setText(date.toString() + ' --- 3.10%')
-        elif date == QDate(2020, 10, 27):
-            self.ui.label_3.setText(date.toString() + ' --- 2.77%')
-        else:
+        df2 = self.loadTemperatureData()
+        interval = self.calcInterval(df2)
+        pred_probs = self.poisson_probs(interval)
+
+        # assuming transformer failure just happened today :O
+        td = QDate.currentDate()
+        date_list = []
+        # dont count day 0
+        for i in range(0, len(pred_probs)):
+            d = td.addDays(i)
+            date_list.append(d)
+
+        # index defaults to -1 if date is not in date_list
+        try:
+            dayIndex = date_list.index(date)  # get index of the date
+        except:
+            dayIndex = -1
+
+        # dont show day 0 prob because its calculated at 0% lol
+        if dayIndex > 0:  # date clicked has a probability to display
+            self.ui.label_3.setText(date.toString() + ' - ' + str(round(pred_probs[dayIndex] * 100, 2)) + '%')
+        else:  # date does not have a probability
             self.ui.label_3.setText(date.toString())
 
     def showpage0(self):
@@ -143,67 +184,68 @@ class MainWindow():
         pt.importCSV(filename=import_file_path, dialog=True)
         root.mainloop()
 
-    #def Enlarge_2(self):
-        # = pd.read_csv('mvlv-transformer.csv',
-                           #usecols=['active_power', 'current', 'voltage', 'active_power', 'reactive_power',
-                                    #'apparent_power'])
-        #f, axes = plt.subplots(1, 1, figsize=(8, 4))
+    # def Enlarge_2(self):
+    # = pd.read_csv('mvlv-transformer.csv',
+    # usecols=['active_power', 'current', 'voltage', 'active_power', 'reactive_power',
+    # 'apparent_power'])
+    # f, axes = plt.subplots(1, 1, figsize=(8, 4))
 
-        #if self.ui.checkBox_1.isChecked():
-            #data.head(10000).plot(y=['active_power', 'reactive_power', 'apparent_power'], ax=axes)
-            #plt.show()
-        #if self.ui.checkBox_2.isChecked():
-            #data.head(20000).plot(y=['current'], ax=axes)
-            #plt.show()
-        #if self.ui.checkBox_3.isChecked():
-            #data.head(20000).plot(y=['voltage'], ax=axes)
-            #plt.show()
+    # if self.ui.checkBox_1.isChecked():
+    # data.head(10000).plot(y=['active_power', 'reactive_power', 'apparent_power'], ax=axes)
+    # plt.show()
+    # if self.ui.checkBox_2.isChecked():
+    # data.head(20000).plot(y=['current'], ax=axes)
+    # plt.show()
+    # if self.ui.checkBox_3.isChecked():
+    # data.head(20000).plot(y=['voltage'], ax=axes)
+    # plt.show()
 
     # def execute(self, filepath):
     # QProcess.startDetached(filepath)
 
     # def display_Power(self):
-        # if self.ui.checkBox.isChecked():
-            # if self.ui.checkBox_2.isChecked():
-                # self.ui.checkBox_2.setChecked(False)
+    # if self.ui.checkBox.isChecked():
+    # if self.ui.checkBox_2.isChecked():
+    # self.ui.checkBox_2.setChecked(False)
 
-            # if self.ui.checkBox_3.isChecked():
-                # self.ui.checkBox_3.setChecked(False)
+    # if self.ui.checkBox_3.isChecked():
+    # self.ui.checkBox_3.setChecked(False)
 
-        # self.ui.graphicsView_2.setStyleSheet("border-image: url(:/page 2/All 3 Power.jpeg);")
-        # self.ui.graphicsView_3.setStyleSheet("border-image: url(:/page2.1/Active.jpeg);")
-        # self.ui.checkBox.stateChanged.connect(self.Un_Display_pic)
+    # self.ui.graphicsView_2.setStyleSheet("border-image: url(:/page 2/All 3 Power.jpeg);")
+    # self.ui.graphicsView_3.setStyleSheet("border-image: url(:/page2.1/Active.jpeg);")
+    # self.ui.checkBox.stateChanged.connect(self.Un_Display_pic)
 
     # def display_Current(self):
-        # if self.ui.checkBox_2.isChecked():
-            # if self.ui.checkBox.isChecked():
-                # self.ui.checkBox.setChecked(False)
+    # if self.ui.checkBox_2.isChecked():
+    # if self.ui.checkBox.isChecked():
+    # self.ui.checkBox.setChecked(False)
 
-            # if self.ui.checkBox_3.isChecked():
-                # self.ui.checkBox_3.setChecked(False)
+    # if self.ui.checkBox_3.isChecked():
+    # self.ui.checkBox_3.setChecked(False)
 
-        # self.ui.graphicsView_2.setStyleSheet("border-image: url(:/page 2.4/Current 20k.jpeg);")
-        # self.ui.graphicsView_3.setStyleSheet("border-image: url(:/page 2.3/Current 10k.jpeg);")
-        # self.ui.checkBox_2.stateChanged.connect(self.Un_Display_pic)
+    # self.ui.graphicsView_2.setStyleSheet("border-image: url(:/page 2.4/Current 20k.jpeg);")
+    # self.ui.graphicsView_3.setStyleSheet("border-image: url(:/page 2.3/Current 10k.jpeg);")
+    # self.ui.checkBox_2.stateChanged.connect(self.Un_Display_pic)
 
     # def display_Voltage(self):
-        # if self.ui.checkBox_3.isChecked():
-            # if self.ui.checkBox.isChecked():
-                # self.ui.checkBox.setChecked(False)
+    # if self.ui.checkBox_3.isChecked():
+    # if self.ui.checkBox.isChecked():
+    # self.ui.checkBox.setChecked(False)
 
-            # if self.ui.checkBox_2.isChecked():
-                # self.ui.checkBox_2.setChecked(False)
+    # if self.ui.checkBox_2.isChecked():
+    # self.ui.checkBox_2.setChecked(False)
 
-        # self.ui.graphicsView_2.setStyleSheet("border-image: url(:/page 2.6/Voltage 20k.jpeg);")
-        # self.ui.graphicsView_3.setStyleSheet("border-image: url(:/page 2.5/Voltage 10k.jpeg);")
-        # self.ui.checkBox_3.stateChanged.connect(self.Un_Display_pic)
+    # self.ui.graphicsView_2.setStyleSheet("border-image: url(:/page 2.6/Voltage 20k.jpeg);")
+    # self.ui.graphicsView_3.setStyleSheet("border-image: url(:/page 2.5/Voltage 10k.jpeg);")
+    # self.ui.checkBox_3.stateChanged.connect(self.Un_Display_pic)
 
     # def Un_Display_pic(self):
-        # self.ui.graphicsView_3.setStyleSheet("background: ")
-        # self.ui.graphicsView_2.setStyleSheet("background: ")
-        # self.ui.checkBox.stateChanged.connect(self.display_Power)
-        # self.ui.checkBox_2.stateChanged.connect(self.display_Current)
-        # self.ui.checkBox_3.stateChanged.connect(self.display_Voltage)
+    # self.ui.graphicsView_3.setStyleSheet("background: ")
+    # self.ui.graphicsView_2.setStyleSheet("background: ")
+    # self.ui.checkBox.stateChanged.connect(self.display_Power)
+    # self.ui.checkBox_2.stateChanged.connect(self.display_Current)
+    # self.ui.checkBox_3.stateChanged.connect(self.display_Voltage)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
